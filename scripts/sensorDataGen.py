146934445a1b4399aa365269200550b9df3a24da
nicolekjiang <nicolekjiang@gmail.com>
2015-07-21 19:58:05 -0400

    finished Dandelion generator

--- scripts/sensorDataGen.py
+++ scripts/sensorDataGen.py
@@ -10,7 +10,7 @@ import threading
 
 
 dandelion = {}
-dandNum = 2
+dandNum = 27
 
 class Dandelion: 
 
@@ -43,11 +43,23 @@ class Dandelion:
 	def showHumidity(self):
 		return self.humidity
 
+	def setHumidity(self,h):
+		self.humidity = h
+
 	def showSoilTemp(self, num):
 		return self.soiltemp[num]
-		
+	def setSoilTemp(self, n1,n2,n3):
+		self.soiltemp[0] = n1
+		self.soiltemp[1] = n2
+		self.soiltemp[2] = n3
+
 	def showMois(self, num):
 		return self.mois[num]
+
+	def setMois(self, n1,n2,n3):
+		self.mois[0] = n1
+		self.mois[1] = n2		
+		self.mois[2] = n3
 
 	def showBatteryLevel(self):
 		return self.batteryLevel
@@ -91,7 +103,6 @@ def updateDand(dandNum):
 
 #generate sensor data for dandelion
 def genDandSensorData(id):
-	print('Dandelion' + str(id) + ' is to be updated' )
 	d = dandelion.get(id) #get particular dandelion
 	counter = d.showCounter()
 	if counter >12:
@@ -99,11 +110,17 @@ def genDandSensorData(id):
 	batteryLevel = d.showBatteryLevel()
 	status = d.showStatus()
 	air = d.showAirTemp()
-
+	humidity = d.showHumidity()
+	s1 = d.showSoilTemp(0)
+	s2 = d.showSoilTemp(1)
+	s3 = d.showSoilTemp(2)
+	m1 = d.showMois(0)
+	m2 = d.showMois(1)
+	m3 = d.showMois(2)
 	# batterlevel
 	randnum = randint(0,1) #for battery decrement
 	if randnum == 1:
-		batteryLevel = batteryLevel - randint(1,5)
+		batteryLevel = batteryLevel - randint(1,4)
 	
 	# state
 	if batteryLevel < 5:
@@ -111,42 +128,70 @@ def genDandSensorData(id):
 	else: 
 		status = 'on'
 
-	#air temperature 
+	if batteryLevel <= 0:
+		batteryLevel = 100
+
+	#all values moved by counter 
 	if counter < 5: 
 		air = air + randint(0,4)
+		humidity = humidity + randint(0,20)
+		s1 = s1 + randint(0,2)
+		s2 = s2 + randint(0,2)
+		s3 = s3 + randint(0,2)
+		m1 = m1 + randint(0,10)
+		m2 = m2 + randint(0,10)
+		m3 = m3 + randint(0,10)
+
 	elif counter in range(5,9):
 		air = air + randint(-2, 2)
+		humidity = humidity + randint(-10,10)
+		s1 = s1 + randint(-1,1)
+		s2 = s2 + randint(-1,1)
+		s3 = s3 + randint(-1,1)
+		m1 = m1 + randint(-5,5)
+		m2 = m2 + randint(-5,5)
+		m3 = m3 + randint(-5,5)
 	else:
 		air = air - randint(0,4)
+		humidity = humidity -randint(10,20)
+		s1 = s1 - randint(0,2)
+		s2 = s2 - randint(0,2)
+		s3 = s3 - randint(0,2)
+		m1 = m1 - randint(0,10)
+		m2 = m2 - randint(0,10)
+		m3 = m3 - randint(0,10)
+
+
+	if humidity <= 0:
+		humidity = 0
+	if air <= 0:
+		air = 0
 
 	#setting values 
 	d.setBatteryLevel(batteryLevel)
 	d.setStatus(status)
 	d.setAirTemp(air)
 	d.setCounter(counter+1)
+	d.setHumidity(humidity)
+	d.setSoilTemp(s1,s2,s3)
+	d.setMois(m1,m2,m3)
 
-	print('Dandelion' + str(id) + ' BatteryLevel: ' + str(batteryLevel) + ' Status: ' + str(status)
-		+ 'AirTemperature: ' + str(air))
-
-
-
-	# airTemp = randint(21,32)
-	# humidity = randint(0,100)
-	# soilTemp1 = randint(12,32)
-	# soilTemp2 = randint(12,32)
-	# soilTemp3 = randint(12,32)
-	# mois1 = randint(0,100)
-	# mois2 = randint(0,100)
-	# mois3 = randint(0,100)
-	# batteryLevel = randint(0,100)
-	# irradiance = randint(0,100)
+	print('Counter: ' + str(counter) +
+		' Dandelion' + str(id) + 
+		' BatteryLevel: ' + str(batteryLevel) + 
+		' Status: ' + str(status) + 
+		' AirTemperature: ' + str(air) + 
+		' Humidity: ' + str(humidity) +
+		' Soil Temperature: ' + str(s1) + " " + str(s2) + " " + str(s3) + " " +
+		' Moisture: ' + str(m1) + " " + str(m2) + " " + str(m3) + " ")
 
 	#write to file 
-	# with open("sensorData.txt", "a") as myfile:
-	#     myfile.write("\n"+str(date)+","+str(dandelionID)+","+str(sunflowerMAC)+","
-	#     	+str(state)+","+str(airTemp)+","+str(humidity)+","+str(soilTemp1)+","
-	#     	+str(soilTemp2)+","+str(soilTemp3)+","+str(mois1)+","+str(mois2)+","
-	#     	+str(mois3)+","+str(batteryLevel)+","+str(irradiance))
+	with open("sensorData.txt", "a") as myfile:
+	    myfile.write(str(id) + " "
+	    	+str(status)+" "+str(air)+"," + str(humidity)+" "+
+	    	str(s1) + " " + str(s2) + " " + str(s3)+","+ 
+	    	str(m1) + " " + str(m2) + " " + str(m3)+" "+
+	    	str(batteryLevel)+ "\n")
 
 	time.sleep(2) #delay for generating next round of data 
 	runGenerator()
